6810d2a6e2c70c0ac01c6d4766a1babe16e2e7a0
Jaehyun Kim <seojaeinblank@gmail.com>
2022-02-12 21:02:57 +0900

    feat : mp4_to_ts

--- server.js
+++ server.js
@@ -68,9 +68,16 @@ const nodeMediaServerConfig = {
     tasks: [
       {
         app: 'live',
-        ac: 'aac',
-        mp4: true,
-        mp4Flags: '[movflags=faststart]',
+        vc: "copy",
+        vcParam: [],
+        ac: "aac",
+        acParam: ['-ab', '64k', '-ac', '1', '-ar', '44100'],
+        rtmp:true,
+        rtmpApp:'live2',
+        hls: true,
+        hlsFlags: '[hls_time=10:hls_list_size=3:hls_flags=delete_segments]',
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
       },
     ],
   },
